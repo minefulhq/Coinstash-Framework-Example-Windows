6712be1848bd358fcdf5b4b78204402b89c519cd
Vasil Urda <vasilurda.sun@gmail.com>
2018-06-13 15:09:27 +0400

    xmrstak,  xmrig, cn-cpu-miner testings.

---
+++ DllTest/Form1.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace DllTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 159);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(121, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(141, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Hash Rate:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(290, 49);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "CPU Usage:";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Monero7",
+            "Graft",
+            "Electroneum",
+            "Sumokoin",
+            "Karbo",
+            "Intense"});
+            this.comboBox1.Location = new System.Drawing.Point(12, 46);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 3;
+            this.comboBox1.Text = "Monero7";
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(12, 86);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 4;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            50,
+            0,
+            0,
+            0});
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(144, 87);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(50, 17);
+            this.checkBox1.TabIndex = 5;
+            this.checkBox1.Text = "AMD";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(200, 87);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(62, 17);
+            this.checkBox2.TabIndex = 6;
+            this.checkBox2.Text = "NVIDIA";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "3333",
+            "5555",
+            "7777"});
+            this.comboBox2.Location = new System.Drawing.Point(12, 121);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 21);
+            this.comboBox2.TabIndex = 7;
+            this.comboBox2.Text = "3333";
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Items.AddRange(new object[] {
+            "xmr-stak",
+            "xmrig",
+            "cp-cpu-miner"});
+            this.comboBox3.Location = new System.Drawing.Point(12, 12);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(344, 21);
+            this.comboBox3.TabIndex = 8;
+            this.comboBox3.Text = "xmr-stak";
+            this.comboBox3.SelectedIndexChanged += new System.EventHandler(this.comboBox3_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(445, 192);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Windows Coinstash";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox3;
+    }
+}
+
---
+++ DllTest/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Coinstash_Windows;
+
+namespace DllTest
+{
+    public partial class Form1 : Form
+    {
+        bool isStarted = false;
+        Timer timer;
+        public Form1()
+        {
+            InitializeComponent();
+            FormClosing += Form1_FormClosing;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Coinstash.stopMining();
+            //Console.WriteLine("YES");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            Console.WriteLine(System.Diagnostics.Process.GetCurrentProcess().Id);
+            Coinstash.setParentProcessId(System.Diagnostics.Process.GetCurrentProcess().Id);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!isStarted)
+            {
+                Coinstash.setNvidia(!checkBox2.Checked);
+                Coinstash.setAMD(!checkBox1.Checked);
+                Console.WriteLine(comboBox3.Text);
+                if (comboBox3.Text == "xmr-stak")
+                {
+                    Coinstash.setMinerType(Coinstash.MinerType.XMRSTAK);
+                }
+                else if (comboBox3.Text == "xmrig")
+                {
+                    Coinstash.setMinerType(Coinstash.MinerType.XMRIG);
+                }
+                else
+                {
+                    Coinstash.setMinerType(Coinstash.MinerType.CNCPUMINER);
+                }
+                Coinstash.setCPULimit((int)numericUpDown1.Value);
+                Coinstash.startMining(int.Parse(comboBox2.Text), "x", SystemInfo.logicalCores() / 2, "warn", comboBox1.Text, "08b0e9578676cd5f7b8c1670d095389db786061d");
+                button1.Text = "Stop";
+                timer = new Timer();
+                timer.Interval = 1000;
+                timer.Tick += new EventHandler(timer_tick);
+                timer.Enabled = true;
+                timer.Start();
+            }
+            else
+            {
+                Coinstash.stopMining();
+                button1.Text = "Start";
+                timer.Stop();
+            }
+            isStarted = !isStarted;
+
+        }
+
+        private void timer_tick(object sender, EventArgs e)
+        {
+            label1.Text = "Hash Rate: " + Coinstash.getHashRate().ToString() + " Hash/S\n" +
+                "Accepted: " + Coinstash.getAccepted().ToString() + " \n";
+            label2.Text = "CPU Usage: " + SystemInfo.cpuUsage().ToString() + " %\n";
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
